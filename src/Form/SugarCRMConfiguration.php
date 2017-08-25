cd0cf2fe509923161e89ab1438c2fa356ce03b64
Nikola Bintev <nikola.bintev@ffwagency.com>
2017-08-25 10:13:58 +0300

    Remove the validation of Sugar CRM configuration form

--- src/Form/SugarCRMConfiguration.php
+++ src/Form/SugarCRMConfiguration.php
@@ -4,7 +4,6 @@ namespace Drupal\webform_sugarcrm\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\webform_sugarcrm\SugarCrmClient;
 
 /**
  * Class SugarCRMConfiguration
@@ -56,47 +55,9 @@ class SugarCRMConfiguration extends FormBase{
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $config = \Drupal::configFactory()->getEditable('webform_sugarcrm.sugarcrm_configuration');
-
-    $url = $form_state->getValue('sugarcrm_restful_url');
-    $user = $form_state->getValue('sugarcrm_service_user');
-    $password = $form_state->getValue('sugarcrm_service_password');
-
-    if (!empty($password)) {
-      $password = md5($password);
-    }
-    else {
-      $password = $config->get('password');
-    }
-
-    // Test the connection settings.
-    $params = array(
-      'url' => $url,
-      'user' => $user,
-      'password' => $password,
-    );
-
-    try {
-      $client = new SugarCrmClient($params);
-      if (!empty($client->getSession()->id)) {
-        drupal_set_message(t('A connection to the SugarCRM instance could be established.'));
-      }
-      else {
-        drupal_set_message(t('Unable to establish a session to the SugarCRM instance. Please check you credentials.'), 'warning');
-      }
-    }
-    catch (\Exception $exc) {
-      \Drupal::logger('webform_sugarcrm')->error($exc->getMessage());
-      drupal_set_message(t("It seems like your credentials isn't valid, or the SugarCRM webservice instance is down."), 'error');
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = \Drupal::configFactory()->getEditable('webform_sugarcrm.sugarcrm_configuration');
+
     $url = $form_state->getValue('sugarcrm_restful_url');
     $user = $form_state->getValue('sugarcrm_service_user');
     $pass = $form_state->getValue('sugarcrm_service_password');
@@ -112,6 +73,19 @@ class SugarCRMConfiguration extends FormBase{
     $config->set('password', $pass);
 
     $config->save(TRUE);
+
+    try {
+      // Initialize client service and try to login in the CRM.
+      $client = \Drupal::service('webform_sugarcrm.sugarcrm_client');
+      $client->login();
+
+      drupal_set_message(t('A connection to the SugarCRM instance could be established.'));
+    }
+    catch (\Exception $exc) {
+      \Drupal::logger('webform_sugarcrm')->error($exc->getMessage());
+      drupal_set_message(t("It seems like your credentials isn't valid, or the SugarCRM webservice instance is down."), 'error');
+    }
+
   }
 
 }
